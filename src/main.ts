b04a49e63f8e1f666fd01204fec161506c775060
poluektovdan <127665642+poluektovdan@users.noreply.github.com>
2024-05-12 17:06:08 +0300

    Улучшена DI: установлены зависимости через интерфейсы с использованием reflect-metadata и inversify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
+import { Container, ContainerModule, interfaces } from 'inversify'
 import { App } from './app'
-import { LoggerService } from './logger/logger.service'
-import { UserController } from './users/users.controller'
 import { ExeptionFilter } from './errors/exeption.filter'
+import { IExeptionFilter } from './errors/exeption.filter.interface'
+import { ILogger } from './logger/logger.interface'
+import { LoggerService } from './logger/logger.service'
+import { TYPES } from './types'
+import { UserController } from './users/user.controller'
+import { IUserController } from './users/user.controller.interface'
 
-async function bootstrap() {
-  const logger = new LoggerService()
-  const app = new App(
-    logger,
-    new UserController(logger),
-    new ExeptionFilter(logger)
-  )
-  await app.init()
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+  bind<ILogger>(TYPES.ILogger).to(LoggerService)
+  bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter)
+  bind<IUserController>(TYPES.UserController).to(UserController)
+  bind<App>(TYPES.Application).to(App)
+})
+
+function bootstrap() {
+  const appContainer = new Container()
+  appContainer.load(appBindings)
+  const app = appContainer.get<App>(TYPES.Application)
+  app.init()
+  return { appContainer, app }
 }
 
-bootstrap()
+export const { app, appContainer } = bootstrap()
