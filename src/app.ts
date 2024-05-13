92a4cfa924ec4ae53ac8254ee8fa29fbbb2deceb
poluektovdan <127665642+poluektovdan@users.noreply.github.com>
2024-05-13 17:23:52 +0300

    Добавлено: линтинг кода с помощью prettier и eslint; nodemon для автоматического перезапуска сервера и отладки

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,39 @@
-import express, { Express } from 'express'
-import { Server } from 'http'
-import { ILogger } from './logger/logger.interface'
-import { inject, injectable } from 'inversify'
-import { TYPES } from './types'
-import { UserController } from './users/user.controller'
-import { IExeptionFilter } from './errors/exeption.filter.interface'
-import 'reflect-metadata'
+import express, { Express } from 'express';
+import { Server } from 'http';
+import { ILogger } from './logger/logger.interface';
+import { inject, injectable } from 'inversify';
+import { TYPES } from './types';
+import { UserController } from './users/user.controller';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import 'reflect-metadata';
 
 @injectable()
 export class App {
-  app: Express
-  server: Server
-  port: number
+	app: Express;
+	server: Server;
+	port: number;
 
-  constructor(
-    @inject(TYPES.ILogger) private logger: ILogger,
-    @inject(TYPES.UserController) private userController: UserController,
-    @inject(TYPES.ExeptionFilter) private exeptionFilter: IExeptionFilter
-  ) {
-    this.app = express()
-    this.port = 8000
-  }
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExeptionFilter) private exeptionFilter: IExeptionFilter
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
 
-  useRoutes() {
-    this.app.use('/users', this.userController.router)
-  }
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
+	}
 
-  useExeptionFilters() {
-    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
-  }
+	useExeptionFilters(): void {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
 
-  public async init() {
-    this.useRoutes()
-    this.useExeptionFilters()
-    this.server = this.app.listen(this.port)
-    this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
-  }
+	public async init(): Promise<void> {
+		this.useRoutes();
+		this.useExeptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
 }
