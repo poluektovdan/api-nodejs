d1a639ee6a4076149acafa6758a3d86c339a77b5
poluektovdan <127665642+poluektovdan@users.noreply.github.com>
2024-05-16 12:34:51 +0300

    Улучшение архитектуры приложения: добавлены DTO логина и регистрации пользователя для стандартизации контрактов между фронтэндом и бэкэндом и дальнейшей валидации данных; глобально на уровне инициации приложения добавлен middleware body-parser для парсинга данных из тела запроса; для описания бизнес-логики работы с пользователями добавлена entity пользователя и сервис, который описывает логику работы с ней; добавлен middleware валидации данных, передаваемых в контроллер

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { LoggerService } from './logger/logger.service';
 import { TYPES } from './types';
 import { UserController } from './users/user.controller';
 import { IUserController } from './users/user.controller.interface';
+import { UserService } from './users/users.service';
+import { IUserService } from './users/users.service.interface';
 
 export interface IBootstrapReturn {
 	appContainer: Container;
@@ -17,6 +19,7 @@ export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.ILogger).to(LoggerService);
 	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
 	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserService).to(UserService);
 	bind<App>(TYPES.Application).to(App);
 });
 
